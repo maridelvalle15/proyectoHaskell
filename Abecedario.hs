f8c0b2c6214c99ea5dd22dfd9f1a082c2611fb1a
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-05-06 20:31:57 -0430

    Agregando mas operadores

---
+++ Abecedario.hs
@@ -0,0 +1,81 @@
+module Abecedario where 
+data Term = Var Char | Neg Term |Or Term Term | And Term Term | Imp Term Term |
+			Equiv Term Term | Inequiv Term Term | Igual Term Term |
+			Truee | Falsee
+a:: Term
+a = Var 'a'
+
+b:: Term
+b = Var 'b'
+
+c:: Term
+c = Var 'c'
+
+d:: Term
+d = Var 'd'
+
+e:: Term
+e = Var 'e'
+
+f:: Term
+f = Var 'f'
+
+g:: Term
+g = Var 'g'
+
+h:: Term
+h = Var 'h'
+
+i:: Term
+i = Var 'i'
+
+j:: Term
+j = Var 'j'
+
+k:: Term
+k = Var 'k'
+
+l:: Term
+l = Var 'l'
+
+m:: Term
+m = Var 'm'
+
+n:: Term
+n = Var 'n'
+
+o:: Term
+o = Var 'o'
+
+p:: Term
+p = Var 'p'
+
+q:: Term
+q = Var 'q'
+
+r:: Term
+r = Var 'r'
+
+s:: Term
+s = Var 's'
+
+t:: Term
+t = Var 't'
+
+u:: Term
+u = Var 'u'
+
+v:: Term
+v = Var 'v'
+
+w:: Term
+w = Var 'w'
+
+x:: Term
+x = Var 'x'
+
+y:: Term
+y = Var 'y'
+
+z:: Term
+z = Var 'z'
---
+++ Definiciones.hs
@@ -0,0 +1,78 @@
+module Definiciones where 
+import Abecedario 
+
+--terminos validos
+true :: Term
+true = Truee
+false :: Term
+false = Falsee
+
+--operadores
+neg :: Term -> Term
+neg x = Neg x
+infix 8 \/
+(\/) :: Term -> Term -> Term
+x \/ y = Or x y
+infix 7 /\
+(/\) :: Term -> Term -> Term
+x /\ y = And x y
+infix 6 ==>
+(==>) :: Term -> Term -> Term
+x ==> y = Imp x y
+infix 5 <==>
+(<==>) :: Term -> Term -> Term
+x <==> y =  Equiv x y
+infix 4 !<==>
+(!<==>) :: Term -> Term -> Term
+x !<==> y =  Inequiv x y
+
+{-
+infix 6 ===
+(===) :: Term -> Term -> Equation
+x === y = Igual x y
+--infix 9 =:
+(=:) :: Term -> Term -> Sust     
+a =: b = sust exp (a,b)
+-}
+{-
+--sustitucion
+--sust :: Term -> (Term,Term) -> Term
+--sust (Var s1) (Var s2, term2) = if s1==s2 then term2 else (Var s1)
+--sust (Sum term1 term2) (var,term) = Sum (sust term1 (var,term)) (sust term2 (var,term))
+-}
+
+showTerm :: Term -> String
+showTerm(Var i) = [i]
+--Mostrar \/
+showTerm(Or (Var i)(Var j)) = showTerm(Var i) ++ " \\/ " ++ showTerm(Var j)
+showTerm(Or (Var i) t) = showTerm(Var i) ++ " \\/ (" ++ showTerm(t) ++ ")"
+showTerm(Or t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " \\/ " ++ showTerm(Var i) 
+showTerm (Or t1 t2) = "(" ++ showTerm t1 ++ ") \\/ (" ++ showTerm t2 ++ ")"
+ --Mostrar /\
+showTerm(And (Var i)(Var j)) = showTerm(Var i) ++ " /\\ " ++ showTerm(Var j)
+showTerm(And (Var i) t) = showTerm(Var i) ++ " /\\ (" ++ showTerm(t) ++ ")"
+showTerm(And t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " /\\ " ++ showTerm(Var i) 
+showTerm (And t1 t2) = "(" ++ showTerm t1 ++ ") /\\ (" ++ showTerm t2 ++ ")"
+ --Mostrar ==>
+showTerm(Imp (Var i)(Var j)) = showTerm(Var i) ++ " ==> " ++ showTerm(Var j)
+showTerm(Imp (Var i) t) = showTerm(Var i) ++ " ==> (" ++ showTerm(t) ++ ")"
+showTerm(Imp t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " ==> " ++ showTerm(Var i) 
+showTerm (Imp t1 t2) = "(" ++ showTerm t1 ++ ") ==> (" ++ showTerm t2 ++ ")"
+ --Mostrar <==>
+showTerm(Equiv (Var i)(Var j)) = showTerm(Var i) ++ " <==> " ++ showTerm(Var j)
+showTerm(Equiv (Var i) t) = showTerm(Var i) ++ " <==> (" ++ showTerm(t) ++ ")"
+showTerm(Equiv t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " <==> " ++ showTerm(Var i) 
+showTerm (Equiv t1 t2) = "(" ++ showTerm t1 ++ ") <==> (" ++ showTerm t2 ++ ")"
+ --Mostrar !<==>
+showTerm(Inequiv (Var i)(Var j)) = showTerm(Var i) ++ " !<==> " ++ showTerm(Var j)
+showTerm(Inequiv (Var i) t) = showTerm(Var i) ++ " !<==> (" ++ showTerm(t) ++ ")"
+showTerm(Inequiv t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " !<==> " ++ showTerm(Var i) 
+showTerm (Inequiv t1 t2) = "(" ++ showTerm t1 ++ ") !<==> (" ++ showTerm t2 ++ ")"
+--Mostrar Neg
+showTerm (Neg (Var i)) = "¬" ++ showTerm(Var i)
+showTerm (Neg t) = "¬(" ++ showTerm(t) ++")"
+--Mostra true y false
+showTerm(Truee) = "true"
+showTerm(Falsee) = "false"
+
+instance Show Term where show = showTerm

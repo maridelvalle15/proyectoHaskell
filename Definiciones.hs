83602f9f6b2d3858f30037722569267498f042ce
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-05-09 11:47:48 -0430

    Listo sustitucion para todos los operadores

--- Definiciones.hs
+++ Definiciones.hs
@@ -1,28 +1,28 @@
 module Definiciones where 
 import Abecedario 
 
---terminos validos
+-- *** TERMINOS VALIDOS ***
 true :: Term
 true = Truee
 false :: Term
 false = Falsee
 
---operadores
+-- *** OPERADORES ***
 neg :: Term -> Term
 neg x = Neg x
-infix 8 \/
+infixl 8 \/
 (\/) :: Term -> Term -> Term
 x \/ y = Or x y
-infix 7 /\
+infixl 7 /\
 (/\) :: Term -> Term -> Term
 x /\ y = And x y
-infix 6 ==>
+infixr 6 ==>
 (==>) :: Term -> Term -> Term
 x ==> y = Imp x y
-infix 5 <==>
+infixl 5 <==>
 (<==>) :: Term -> Term -> Term
 x <==> y =  Equiv x y
-infix 4 !<==>
+infixl 4 !<==>
 (!<==>) :: Term -> Term -> Term
 x !<==> y =  Inequiv x y
 infix 3 ===
@@ -32,15 +32,29 @@ infix 1 =:
 (=:) :: Term -> Term -> Sust     
 x =: y = Sustit x y 
 
--- sustitucion
+-- *** SUSTITUCION ***
 sust :: Term -> (Sust) -> Term
 sust Truee (Sustit t2 t3) = Truee
 sust Falsee (Sustit t2 t3) = Falsee
 sust (Var i) (Sustit (Var j) (Var k)) = if i==k then (Var j) else (Var i)
 sust (Var i) (Sustit t1 (Var j)) = if i==j then t1 else (Var i)
+-- sustitucion para \/
 sust (Or t1 t2) (Sustit (Var i) (Var j)) = Or (sust t1 ((Var i)=:(Var j))) (sust t2 ((Var i)=:(Var j)))
+sust (Or t1 t2) (Sustit t3 (Var j)) = Or (sust t1 (t3=:(Var j))) (sust t2 (t3=:(Var j)))
+--sustitucion para /\
+sust (And t1 t2) (Sustit (Var i) (Var j)) = And (sust t1 ((Var i)=:(Var j))) (sust t2 ((Var i)=:(Var j)))
+sust (And t1 t2) (Sustit t3 (Var j)) = And (sust t1 (t3=:(Var j))) (sust t2 (t3=:(Var j)))
+--sustitucion para ==>
+sust (Imp t1 t2) (Sustit (Var i) (Var j)) = Imp (sust t1 ((Var i)=:(Var j))) (sust t2 ((Var i)=:(Var j)))
+sust (Imp t1 t2) (Sustit t3 (Var j)) = Imp (sust t1 (t3=:(Var j))) (sust t2 (t3=:(Var j)))
+--sustitucion para <==>
+sust (Equiv t1 t2) (Sustit (Var i) (Var j)) = Equiv (sust t1 ((Var i)=:(Var j))) (sust t2 ((Var i)=:(Var j)))
+sust (Equiv t1 t2) (Sustit t3 (Var j)) = Equiv (sust t1 (t3=:(Var j))) (sust t2 (t3=:(Var j)))
+--sustitucion para !<==>
+sust (Inequiv t1 t2) (Sustit (Var i) (Var j)) = Inequiv (sust t1 ((Var i)=:(Var j))) (sust t2 ((Var i)=:(Var j)))
+sust (Inequiv t1 t2) (Sustit t3 (Var j)) = Inequiv (sust t1 (t3=:(Var j))) (sust t2 (t3=:(Var j)))
 
-
+-- *** SHOW TERMS *** --
 showTerm :: Term -> String
 showTerm(Var i) = [i]
 --Mostrar Neg
@@ -72,12 +86,15 @@ showTerm(Inequiv (Var i)(Var j)) = showTerm(Var i) ++ " !<==> " ++ showTerm(Var 
 showTerm(Inequiv (Var i) t) = showTerm(Var i) ++ " !<==> (" ++ showTerm(t) ++ ")"
 showTerm(Inequiv t (Var i)) = "(" ++ showTerm(t) ++ ")" ++ " !<==> " ++ showTerm(Var i) 
 showTerm (Inequiv t1 t2) = "(" ++ showTerm t1 ++ ") !<==> (" ++ showTerm t2 ++ ")"
---Mostra true y false
+--Mostrar true y false
 showTerm(Truee) = "true"
 showTerm(Falsee) = "false"
 
+-- *** SHOW SUST *** --
 showSust :: Sust -> String
+--Mostrar sustitucion
 showSust(Sustit t1 t2) = showTerm t1 ++ " =: " ++ showTerm t2
 
+-- *** INSTANCE ***
 instance Show Term where show = showTerm
 instance Show Sust where show = showSust

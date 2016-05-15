3e11b60f8749fb2bb7fdb60a7d83fdf508dcd35e
Marisela Del Valle <marisela.delvalle93@gmail.com>
2016-05-15 13:13:30 -0430

    Eliminados archivos innecesarios

--- read_example.hs
+++
@@ -1,7 +0,0 @@
-import System.IO  
-  
-main = do  
-    handle <- openFile "test.txt" ReadMode  
-    contents <- hGetContents handle  
-    putStr contents  
-    hClose handle  
--- theorems.hs
+++
@@ -1,6 +0,0 @@
-prop :: Float -> Equation
-prop num
-	| num == 3.1 = (p <==> q) <===> r === p <==> (q <==> r)
-	| num == 3.2 = (p <==> q) <===> (q <==>p ) === true
-	| num == 3.3 = (p <==> q) <===> q === p
-	| otherwise = error "The statement doesn't exists"
